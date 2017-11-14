e8b2da41cad79283b752bfeeec1279ce33d7fbd5
ykhuang <ykhuang@bhuman.com.cn>
2017-11-14 09:17:59 +0800

    新的代码

--- app/src/main/java/com/stkent/polygondrawingutildemo/MainActivity.java
+++ app/src/main/java/com/stkent/polygondrawingutildemo/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements OnSeekBarChangeLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String newMessage = "新的提交";
+        String newsd = "新的东西";
 
         maxCornerRadius = getResources().getDimension(R.dimen.max_corner_radius);
 
